629f5e67de957b97659730efbb63554d4beef092
Saraswati Panda <saraswati.panda@estpl.in>
2023-09-12 13:08:31 +0530

    frontend pages

---
+++ frontend/src/pages/Auth/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Layout from '../../components/Layouts/Layout'
+import axios from 'axios'
+
+const Login = () => {
+  const [userData,setUserData] = useState({email:"",password:""})
+
+  const handleChange = (e) => {
+    setUserData({...userData,[e.target.id]: e.target.value})
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    const data ={
+      email:userData.email,
+      password:userData.password
+    }
+    try {
+      const res = await axios.post('http://localhost:8080/auth/login', data)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <Layout title={'Login - Ecommer App'}>
+      <div className='register'>
+       <h1>login</h1>
+      <form onSubmit={handleSubmit}>
+        <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" onChange={handleChange}/>
+       </div>
+       <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" onChange={handleChange}/>
+       </div>
+       <button type="submit" class="btn btn-primary d-grid col-3 mx-auto">Submit</button>
+      </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default Login
