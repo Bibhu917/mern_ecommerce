09275d7770505123c50252de3f17abb831cb7db3
Saraswati Panda <saraswati.panda@estpl.in>
2023-09-12 11:59:35 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dbUrl = require('./config/db')
+const connection = require('./config/db')
 const authRoute = require('./routes/authroute')
 const categoryRoute = require('./routes/category')
 const productRouter = require('./routes/product')
@@ -12,15 +12,15 @@ dotenv.config();
 app.use(express.json())
 
 //routes
-app.use('/api/v1/auth',authRoute)
-app.use('/api/v1/category',categoryRoute)
-app.use('/api/v1/product',productRouter)
+app.use('/api/auth',authRoute)
+app.use('/api/category',categoryRoute)
+app.use('/api/product',productRouter)
 
 app.use('/',async(req,res)=>{
     return res.send({message:"Hello World from Index"})
 })
 
 app.listen(process.env.PORT,async()=>{
-    await dbUrl();
-   console.log("Listen on Port 8000")
+    await connection();
+   console.log(`Listen on Port ${process.env.PORT}`)
 })
